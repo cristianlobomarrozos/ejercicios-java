e35a69c25a8e600bdc366bb04d1f1c799521b51c
Cristian Lobo Marrozos <cristian.lobo.marrozos@gmail.com>
2018-11-29 11:12:03 +0100

    actualización

--- Tema 7/Arrays bidimensionales/ejercicio10.java
+++ Tema 7/Arrays bidimensionales/ejercicio10.java
@@ -26,45 +26,100 @@ public class ejercicio10{
       }
     }
     
-    if(ficha == "O"){
-      fila = (int)(Math.random()*4);
-      columna = (int)(Math.random()*4);
-      for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-          tablero[fila][columna] = "X";
-          tablero[i][j] = tablero[fila][columna];
-        }
-      }
-    }
     
-    for(int i=0; i<3; i++) {
-      for(int j=0; j<3; j++){
-        System.out.printf("%3d",tablero[i][j]);
-      }
-      System.out.println();
-    }
-    
-    System.out.println();
     
     boolean victoria = false;
     boolean tableroLleno = false;
     
-    /*do{
+    do{
+      /*
+      if(ficha == "O"){
+        for(int i=0; i<3; i++){
+          for(int j=0; j<3; j++){
+            fila = (int)(Math.random()*4);
+            columna = (int)(Math.random()*4);
+            if((fila == i) && (columna == j) && (tablero[i][j] == " ")){
+              tablero[i][j] = "X";
+            }
+          }
+        }
+      }else{
+        for(int i=0; i<3; i++){
+          for(int j=0; j<3; j++){
+            fila = (int)(Math.random()*4);
+            columna = (int)(Math.random()*4);
+            if((fila == i) && (columna == j) && (tablero[i][j] == " ")){
+              tablero[i][j] = "O";
+            }
+          }
+        }
+      }   */
       
-      
+      if(ficha == "O"){
+        for(int i=0; i<3; i++){
+          for(int j=0; j<3; j++){
+            fila = (int)(Math.random()*3);
+            columna = (int)(Math.random()*3);
+            
+            if((fila == i) && (columna == j) && (tablero[i][j] == " ")){
+              tablero[i][j] = "X";
+            }
+          }
+        }
+      } else{
+        for(int i=0; i<3; i++){
+          for(int j=0; j<3; j++){
+            fila = (int)(Math.random()*3);
+            columna = (int)(Math.random()*3);
+            
+            if((fila == i) && (columna == j) && (tablero[i][j] == " ")){
+              tablero[i][j] = "O";
+            }
+          }
+        }
+      }
       
       //pedimos donde quiere poner su ficha
-      System.out.print("Introduzca la fila en la que quiere colocar la ficha: " + ficha);
+      System.out.print("Introduzca la fila en la que quiere colocar la ficha " + ficha + " : ");
       fila = Integer.parseInt(System.console().readLine());
-      System.out.print("Introduzca la columna en la que quiere colocar la ficha: ");
+      System.out.print("Introduzca la columna en la que quiere colocar la ficha " + ficha + " : ");
       columna = Integer.parseInt(System.console().readLine());
       
       
       
+      for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++){
+          if((fila == i) && (columna == j) && (tablero[i][j] == " ")){
+            tablero[i][j] = ficha;
+          }
+        }
+      }
       
       
+      for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++){
+          System.out.print(tablero[i][j] + "  ");
+        }
+        System.out.println();
+      }
+    
+      System.out.println();
       
-    }while((!victoria) || (!tableroLleno));*/
+      for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+          if((ficha == "X")){
+            if(((i == 0) && (j == 0) && (((tablero[i][j] == tablero[i][j+1]) && (tablero[i][j] == tablero[i][j+2]))
+                || ((tablero[i][j] == tablero[i+1][j]) && (tablero[i][j] == tablero[i+2][j])) 
+                || ((tablero[i][j] == tablero[i][j+1]) && (tablero[i][j] == tablero[i][j+2]))))){
+              victoria = true;
+            }
+          }else{
+            
+          }
+        }
+      }
+      
+    }while((!victoria) || (!tableroLleno));
     
     
     
