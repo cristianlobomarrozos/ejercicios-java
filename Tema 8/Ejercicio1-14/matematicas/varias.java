d4942e647c1967e6794713b2271af48d23bab56c
Cristian Lobo Marrozos <cristian.lobo.marrozos@gmail.com>
2018-12-04 10:34:19 +0100

    actualización

---
+++ Tema 8/Ejercicio1-14/matematicas/varias.java
@@ -0,0 +1,75 @@
+package matematicas;
+
+public class varias{
+  public static boolean esCapicua(int x){
+    
+  }
+  
+  public static boolean esPrimo(long x){
+    for(int i=2; i<x; i++){
+      if((x%i)==0){
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  public static int siguientePrimo(int x){
+    do{
+      x++;
+      
+    }while(!esPrimo(x));
+    
+    return x;
+  }
+  
+  public static int potencia(int x, int y){
+    if(y == 0){
+      return 1;
+    }
+    int z = 1;
+    
+    for(int i=0; i<x; i++){
+      z = z*x;
+    }
+    return z;
+  }
+  
+  public static int digitos(int x){
+    if(x==0){
+     return 1; 
+    }else{
+      int contador = 0;
+      while (x>0){
+        x = x/10;
+        contador++;
+      }
+      return contador;
+    }
+  }
+  
+  public static int voltea(int x){
+    int voltear = 0;
+    
+    while(x>0){
+      voltear = (voltear * 10) + (x%10);
+      x = x/10;
+    }
+    return voltear;
+  }
+  
+  public static int digitoN(int x, int y){
+    int posicion = 1;
+    for(int i=0; i<y; i++){
+      posicion = posicion * 10;
+    }
+    int digito;
+    digito = x%posicion;
+    
+    return digito;
+  }
+  
+  public static int posicionDeDigito(){
+    
+  }
+}
