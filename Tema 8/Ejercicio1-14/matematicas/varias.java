c75a11c0dd0e33d0488fe1f7892cceb2c5c0fb0b
Cristian Lobo Marrozos <cristian.lobo.marrozos@gmail.com>
2018-12-05 14:06:53 +0100

    actualización

--- Tema 8/Ejercicio1-14/matematicas/varias.java
+++ Tema 8/Ejercicio1-14/matematicas/varias.java
@@ -95,7 +95,7 @@ public class varias{
         }
       }
     }
-    return 0;
+    
   }
   
   public static int quitarPorDetras(int x){
--- Tema 8/Ejercicio1-14/prueba.java
+++ Tema 8/Ejercicio1-14/prueba.java
@@ -2,11 +2,11 @@ import matematicas.varias;
 
 public class prueba{
   public static void main(String[] args){
-    int resul = 1234;
+    int resul = 1214;
     int posi = 1;
     
     
-    System.out.print(varias.digitoN(resul, (posi-1)));
+    System.out.print(varias.posicionDeDigito(resul));
     
     
     
