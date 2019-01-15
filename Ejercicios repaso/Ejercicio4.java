c572aae75c7fa2acc8db25853728b95aa5b54142
Cristian Lobo Marrozos <cristian.lobo.marrozos@gmail.com>
2019-01-15 11:05:34 +0100

    actualización

--- Ejercicios repaso/Ejercicio4.java
+++ Ejercicios repaso/Ejercicio4.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Ejercicio4 {
   public static void main(String[] args) {
     int tama;
@@ -7,22 +5,32 @@ public class Ejercicio4 {
     String pal;
     int minimo = 0;
     
-    Scanner s = new Scanner(System.in);
     
-    System.out.print("Introduzca el tamaño del array: ");
+    System.out.print("Introduzca el número de palabras que desea introducir: ");
     tama = Integer.parseInt(System.console().readLine());
     
     String[] palabras = new String[tama];
     int[] posicion = new int[tama];
     
+    boolean noRepetido = false;
     for (int i=0; i<tama; i++) {
       System.out.print("Introduzca la palabra de la posición: " + i + ": ");
-      pal = s.nextLine();
+      pal = System.console().readLine();
       palabras[i] = pal;
       
-      System.out.print("Introduzca el número desde 0 hasta " + (tama-1) + "de la posición " + i + ": ");
-      pos = Integer.parseInt(System.console().readLine());
-      
+      do{
+        System.out.print("Introduzca el número desde 0 hasta " + (tama-1) + " de la posición " + i + ": ");
+        pos = Integer.parseInt(System.console().readLine());
+        System.out.println();
+        for(int j=0; (j<i || noRepetido); j++) {
+          if(pos == posicion[j]) {
+            System.out.println("Número repetido.");
+          } else {
+            noRepetido = true;
+          }
+        }
+        
+      }while (!noRepetido);
     }
     System.out.println();
     
